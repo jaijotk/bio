af590c33baf2d0ad7525330a22f57d5588b2db71
Jaijot Kaur <jaijotkaur@Jaijots-MacBook-Pro.local>
2022-09-13 15:29:48 -0700

    Modify one line and add a fourth line

--- biofolder/bio.R
+++ biofolder/bio.R
@@ -1,3 +1,4 @@
 My name is Jai Jot Kaur.
 I am a third year student at UC Merced majoring in Human Biology.
-I am currently working on applications, working, and am a part time student at three colleges.
+I am currently working on applications, working, studying while making time to spend time with friends and family.
+I enjoy bike riding, watching movies, and scrapbooking.
