fccd291b627a0e9e6ba4afb6e68dd42051e808e3
Jaijot Kaur <jaijotkaur@Jaijots-MacBook-Pro.local>
2022-09-13 15:27:36 -0700

    three line biography

---
+++ biofolder/bio.R
@@ -0,0 +1,3 @@
+My name is Jai Jot Kaur.
+I am a third year student at UC Merced majoring in Human Biology.
+I am currently working on applications, working, and am a part time student at three colleges.
